8190a62493053e2306f1a64c9149b81070bd6d0e
Bryan Z <piantagrane@protonmail.com>
2020-10-22 16:26:10 -0500

    Refactor navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+// React
+import React from "react";
+
+// Third Party
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Navbar = ({ onMenuClick }) => {
+    return (
+        <div className="ui fixed menu">
+            <div className="navbar-title">Audiolibros</div>
+            <div className="right menu navbar-menu-icon">
+                <a href="#" onClick={onMenuClick}>
+                    <FontAwesomeIcon icon={faBars} color="#262525" />
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
